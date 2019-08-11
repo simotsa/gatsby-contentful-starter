7e1d49fcbab9938ea5ddfc4f9fe9655f866b80d3
simotsa <simotsa@yahoo.com>
2019-08-11 18:01:52 -0400

    Upgraded gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,7 @@ module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
     'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
